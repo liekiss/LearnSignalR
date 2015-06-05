951fbc31d2e426ff79cfe527fd8710e75c0ee04c
liekiss <289294189@qq.com>
2015-06-05 14:11:32 +0800

    commit

---
+++ LearningSignalR/LearnSignalR/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LearnSignalR.Startup))]
+namespace LearnSignalR
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
